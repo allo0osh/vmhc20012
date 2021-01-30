e13dd43716d5219ff37580f417af8347810c774e
ali nejabaty <ali.nejabaty@gmail.com>
2021-01-31 00:42:23 +0330

    Initial commit

---
+++ install.bash
@@ -0,0 +1,25 @@
+apt-get install libxss1 libxss-dev libxinerama1 libxinerama-dev openssl curl
+
+apt-get install libatk1.0-0 libatkmm-1.6-1v5 libatk3.0-cil libatk-bridge2.0-0
+
+apt-get install libcairo2 libcairomm-1.0-1v5
+
+apt-get install libtiff5 libtiffxx5
+
+apt-get install libharfbuzz0b
+
+apt-get install libgdk-pixbuf2.0-0 libgdk-pixbuf2.0-bin libgdk-pixbuf2.0-common libgdk3.0-cil
+
+apt-get install libgtkmm-2.4-1v5 libgtkmm-3.0-1v5
+
+apt-get install libpng12-0 #or copy that # and libudev0
+
+apt-get install build-essential libgtk-3-dev
+
+apt-get install libglibmm-2.4-1v5 libglibmm-2.4-dev
+
+apt-get install libatkmm-1.6-1v5 libatkmm-1.6-dev
+
+apt-get install libpangomm-1.4-1v5 libpangomm-1.4-dev
+
+apt-get install libgtkmm-2.4-dev libgtkmm-3.0-dev
